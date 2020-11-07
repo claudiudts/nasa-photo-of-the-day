820c71c80b8f296b1af773dcb1af1552505b1ac7
Claudiuts <sarb.claudiu391@gmail.com>
2020-11-06 18:40:11 -0600

    more work done

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const DateInput = props => (
   <form onSubmit={props.changeDate}>
-    Enter a date (YYYY-MM-DD):
+    Enter a Date (YYYY-MM-DD) :
     <input />
     <input type='submit' />
   </form>
 );
+
+
+
+
 
 export default DateInput;
